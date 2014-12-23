ebeae22f2f2ee960f00fe9e9cf2733f4dedcbab1
unknown <hassan.abul@DD-G9NSMN1.spa.nyc>
2014-12-23 11:33:56 -0500

    New Commit

---
+++ SpaFinder/src/spafinder/com/value/AdminValue.java
@@ -0,0 +1,60 @@
+package spafinder.com.value;
+
+public class AdminValue {
+
+	public String BusinessEmailValue				= "qa.spa29@live.com";
+	
+	
+	public String adminUserNameValue 				=	"qa.test";
+	public String adminPassValue					=	"Finderspa1";
+	
+	public String BusinessFNameValue				= "Sayan";
+	public String BusinessLnameValue				= "Bhuiyan";
+	public String BusinessPostionValue				= "Son Of Bhuiyan";
+	public String BusinessPhoneValue				= "7185597823";
+	public String BusinessPasswordValue				= "Finderspa1";
+	public String BusinessConfirmPasswordValue		= "Finderspa1";
+
+// Business Info:
+	public String SpaNameValue						= "QA Testing";
+	public String SpaNameValueCA					= "CA Testing";
+	public String SpaNameValueUK					= "UK Testing";
+	public String SpaPhoneValue						= "6466950580";
+	public String SpaEmailValue						= "qa.test@spafinder.com";
+	public String SpaTypeValue						= "Day Spa"; 			// drop down
+	public String SpaAddressValue					= "257 Park Ave South";
+	public String SpaCountryValue					= "United States"; 						// drop down
+	public String SpaStateValue						= "New York";  							// Drop down
+	public String SpaCityValue						= "New York City";
+	public String SpaZipCodeValue					= "10010";
+
+	public String spaWebsiteValue					= "http://www.spafinder.com";
+	public String spaDescriptionValue				= "QA-Automation Testing~!!";
+	
+// Business Hours
+	public String spaMondayValue					= "true";
+	public String spaMondayStartValue				= "02:30 PM";
+	public String spaMondayEndValue					= "03:30 PM";
+	
+	public String spaTuesdayValue					= "true";
+	public String spaTuesdayStartValue				= "07:00 AM";
+	public String spaTuesdayEndValue				= "08:00 PM";
+	public String spaTuesdayBreakStartValue			= "01:00 PM";
+	public String spaTuesdayBreakEndsValue			= "02:00 PM";
+	public String spaWednesdayValue					= "true";
+	public String spaImage1							= "C:/Users/hassan.abul/Pictures/1.jpg";
+	
+// Services
+	public String servicePDFValue					= "C:\\Users\\hassan.abul\\Pictures\\TestingAutomation.pdf";
+
+// Deals
+	public String dealTitleValue					= "QA New Deal";
+	public String dealDescriptoinValue				= "It's not easy";
+	
+
+
+
+
+
+}
+
---
+++ SpaFinder/src/spafinder/com/value/OnlineBookingValue.java
@@ -0,0 +1,62 @@
+package spafinder.com.value;
+
+public class OnlineBookingValue {
+	
+	public String SpaEmail									= "qa.test@spafinder.com"; // "qa.spafinder365@gmail.com";
+	public String UKemailValue								= "qa.testuk@hotmail.com";
+	//public String UKemailValue							= "qa.uktest@hotmail.com";
+	public String SpaPassword								= "Finderspa1";
+	public String emailWithNoSavedCard						= "qa.test@hotmail.com";
+		
+// New Cusomters
+	public String MBONewCustomerEmailValue					= "us_mbo4@hotmail.com"; // Change the email address after run the script.
+	public String CAMboNewCustomerEmailValue				= "ca_mbo2@gmail.com";
+	public String UKMboNewCustomerEmailValue				= "uk_mbo3@gmail.com";
+	
+	public String G1NewCustomerEmailValue					= "qa.spa20@gmail.com";
+	public String CAG1NewCustomerEmailValue					= "ca_gone4@gmail.com";
+	public String UKG1NewCustomerEmailValue					= "uk_gone2@gmail.com";
+		
+	public String MBONewCustomerFNameValue					= "SQA";
+	public String MBONewCustomerLNameValue					= "Automation";
+	
+	public String MBONewCustomerPasswordValue				= "Finderspa1";
+	public String MBONewCustomerRetypePasswordValue			= "Finderspa1";
+	public String MBONewCustomerZipCodeValue				= "10010";
+	
+// For UK Only:
+	public String UKCGAMFirstValue 							= "Sayan";
+	public String UKCGAMLastNameValue 						= "Rifa";
+	public String UKCGAMAddressValue						= "5 Chancery Lane";
+	public String UKCGAMCountryValue 						= "United Kingdom";
+	public String UKCGAMCityValue 							= "London";
+	public String UKState									= "London";
+	public String UKCGAMZipCodeValue 						= "EC4A 1BL";
+	public String ukPhoneNumber								= "+44 (0) 870 121 6067";	
+	
+// Already a Member
+	
+
+	public String MBOFNameValue								= "Hasan";
+	public String MBOLNameValue								= "Bhuiyan";
+	public String MBOCountryValue							= "United States";
+	public String MBOStreetAddressValue						= "257 Park Ave South";
+	public String MBOCityValue								= "NYC";
+	public String MBOStateValue								= "New York";
+	public String MBOZipCodeValue							= "10010";
+	public String MBOPhoneNumberValue						= "7185597821";
+	public String MBOCardNumberValue						= "4111111111111111";
+	public String MBOSecurityCodeValue						= "123";
+	public String MBOExpirationMonthValue					= "05";
+	public String MBOExpirationYearValue					= "16";
+	public String MBOCancellationPolicyValue				= "True";
+
+	public String OnlineBookingGCNumber						= "SWB4004496712109390707USD";
+	public String OnlineBookingGCValue						= "50";
+	public String Pass										= "Pass";
+	public String Fail										= "Fail";
+// .....................................................................................................................................
+		
+
+		
+	}
